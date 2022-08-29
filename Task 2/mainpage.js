f4487eefa3d14d5723f0835bbec71860d0f3f2eb
Lachlan Da Silva <lachsilva3@gmail.com>
2022-08-29 07:46:47 +0530

    update task 2

---
+++ Task 2/mainpage.js
@@ -0,0 +1,30 @@
+// TASK 1 (START)
+// call aurthors name using javascript fetch
+document.getElementById('button1').addEventListener('click',loadTxt);
+function loadTxt(){
+    fetch('data.txt')
+    .then(function(response){
+        return response.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('result').innerHTML=data;
+    })
+}
+// Show or hide names
+var toggled =false;
+function toggle(){
+    if (!toggled){
+        toggled=true;
+        document.getElementById("result").style.display="none";
+        return;
+    }
+    if(toggled){
+        toggled=false;
+        document.getElementById("result").style.display="block";
+        return;
+    }
+}
+// TASK 1 (END)
+
+// TASK 2(START)
