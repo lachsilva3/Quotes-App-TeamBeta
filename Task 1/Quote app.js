fd6749f02396cfc35a547f2a0d10ad02c2ab7cb8
Lachlan Da Silva <105099220+lachsilva3@users.noreply.github.com>
2022-08-30 16:18:11 +0530

    task 1 final update

--- Task 1/Quote app.js
+++ Task 1/Quote app.js
@@ -1,24 +1,3 @@
-function btnfunction() {
-  fetch("https://quote-api-app.herokuapp.com/quote")
-    .then((data) => {
-      //console.log(data);
-      return data.json(); //converted to object
-    })
-    .then((ok) => {
-      console.log(ok[0]);
-      let list = "";
-      ok.map((values) => {
-        list += `   
-            <h5>"${values.quote}"</h5>
-            <p><b >&nbsp~${values.author}</b></p>
-            <li>Likes:&nbsp${values.likes}</li>
-            <li>Dislikes:&nbsp${values.dislikes}</li>
-              <br>  <br>     
-        `;
-      });
-      document.getElementById("names").innerHTML = list;
-    });
-}
 function forauthor() {
   fetch("https://quote-api-app.herokuapp.com/author")
     .then((dat) => {
