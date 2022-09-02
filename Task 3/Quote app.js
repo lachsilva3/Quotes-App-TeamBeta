8b9fb7dc6edd1867611321491f9f7e54ed8815da
Lachlan Da Silva <105099220+lachsilva3@users.noreply.github.com>
2022-09-02 12:35:03 +0530

    task 3 done

--- Task 3/Quote app.js
+++ Task 3/Quote app.js
@@ -26,15 +26,49 @@ function btnfunction() {
     .then((ok) => {
       console.log(ok[0]);
       let list = "";
-      ok.map((values) => {
+      ok.map(values => {
          list += `
                   <hr>
             <h5>"${values.quote}"</h5>
-            <p><b >&nbsp~${values.author}</b></p>
+            <p><b >${values.author}</b></p>
             <li>Likes:&nbsp${values.likes}</li>
             <li>Dislikes:&nbsp${values.dislikes}</li>           
         `;
+        return {author:values.author, element:list}
       });
       document.getElementById("names").innerHTML = list;
     });
 }
+
+// Function the search bar
+function search_button() {
+  let users = []
+  const url = "https://quote-api-app.herokuapp.com/quote/search?author="
+  const value = document.getElementById('input').value
+  const api = url + value  
+  fetch(api).then((data3) => {
+        return data3.json(); 
+      })
+      .then((info) => {
+        users = info.map(user => {
+          
+          console.log(info[0]);
+          let list2 = "";
+          info.map((values) => {
+            list2 += `   
+                <h5>"${values.quote}"</h5>
+                <p><b >&nbsp~${values.author}</b></p>
+                <li>Likes:&nbsp${values.likes}</li>
+                <li>Dislikes:&nbsp${values.dislikes}</li>
+                  <br>  <br>     
+            `;
+          });
+          document.getElementById("names").innerHTML = list2;
+        });
+      });
+  }
+
+  function myFunction() {
+    document.getElementById("names").innerHTML = "YOU CLICKED ME!";
+    
+  }
